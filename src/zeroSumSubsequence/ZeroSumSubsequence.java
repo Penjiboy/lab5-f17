609033d545c74fb53c93245f345b50eebb5e91ef
Penjani Chavula <chavulapenjani@gmail.com>
2017-10-11 17:58:36 -0700

    Got the method working and passed all the tests

--- src/zeroSumSubsequence/ZeroSumSubsequence.java
+++ src/zeroSumSubsequence/ZeroSumSubsequence.java
@@ -1,12 +1,40 @@
 package zeroSumSubsequence;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class ZeroSumSubsequence {
 	
 	public static int getMaximumLength(List<Integer> sequence) {
 		// TODO: Implement this method
-		return -1; // Change This
+//		sequence = (ArrayList<Integer>) sequence;
+		int sum = 0;
+		int length = 0;
+		if ( sequence.size() == 0) //If sequence is empty, return 0
+			return 0;
+
+		//For loop to calculate the sum of this sequence
+		for (int count = 0; count < sequence.size(); count++) {
+			sum = sum + sequence.get(count);
+		}
+
+		if (sum == 0) {
+			return sequence.size();
+		} else {
+			List<Integer> smallerList = new ArrayList<Integer>(sequence);
+			List<Integer> sameList = new ArrayList<Integer>(sequence);
+
+			for (int count = 0; count < sameList.size(); count++) {
+				int placeHolder = smallerList.get(count);
+				smallerList.remove(count);
+				int workingLength = getMaximumLength(smallerList);
+				if(workingLength > length)
+					length = workingLength;
+				smallerList.add(count,placeHolder);
+			}
+			return length;
+		}
+
 	}
-	
+
 }
